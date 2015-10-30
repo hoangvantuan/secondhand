3c8183da8d418ffec840c7857e67e38687afd436
hoangvantuan <hoangtuanbka93@gmail.com>
2015-10-30 13:30:08 +0700

    done Insert+ show

--- application/controllers/Category.php
+++ application/controllers/Category.php
@@ -13,5 +13,33 @@ class Category extends CI_Controller
 		$this->mcategory->delete($id);
 		redirect('category','refresh');
 	}
+
+	function show($id){
+		$data['data'] = $this->mcategory->find($id);
+		// var_dump($data['data']->id);
+		$this->load->view('category/show', $data);
+	}
+
+	function add(){
+		$this->load->view('category/add');
+	}
+	function insert(){
+		$category = array('name'=>$this->input->post('name'));
+		$this->mcategory->insert($category);
+		redirect('category','refresh');
+	}
+
+
+	function update($id){
+		$data['data'] = $id;
+		// var_dump($data);
+		$this->load->view('category/edit',$data);	
+	}
+
+	function edit(){
+		$category = array('name'=>$this->input->post('name'));
+		$this->mcategory->update($$this->input->post('id'), $category);
+
+	}
 }
  ?>
--- application/views/category/list.php
+++ application/views/category/list.php
@@ -1,9 +1,10 @@
 <?php 
-
-$this->table->set_heading('Id','Name','Options');
+echo anchor('category/add', 'Add category');
+echo '<br>';
+$this->table->set_heading('Id','Name','Delete','Edit');
 foreach ($category as $value) {
 	# code...
-	$this->table->add_row($value->id,$value->name,anchor('category/delete/'.$value->id, 'Delete', Array('onClick' => "return confirm('Are you sure?')")));
+	$this->table->add_row($value->id,anchor('category/show/'.$value->id,$value->name),anchor('category/delete/'.$value->id, 'Delete', Array('onClick' => "return confirm('Are you sure?')")),anchor('category/update/'.$value->id, 'Edit'));
 }
 
 	echo $this->table->generate();
